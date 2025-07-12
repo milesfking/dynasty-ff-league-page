ccf725a1e610c14240c2840a2303a067949fb70c
milesfking <miles.king@duke.edu>
2025-07-12 16:40:27 -0400

    Activated blog

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,9 +1,9 @@
 /*   STEP 1   */
 export const leagueID = "1182134435053940736"; // your league ID
 export const leagueName = "The Kim Dynasty"; // your league name
-export const dues = 100; // (optional) used in template constitution page
+export const dues = 30; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `

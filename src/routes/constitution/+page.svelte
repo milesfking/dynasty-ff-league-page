7ca51f276d4befbe84d0e9d24caebd76066aa84f
milesfking <miles.king@duke.edu>
2025-07-12 17:28:36 -0400

    Updated constitution

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -96,7 +96,7 @@
 </style>
 
 <div class="constitution">
-    <h1 class="noUnderscore">THE KIM DYNASTY CONSTITUTION</h1>
+    <h1 class="noUnderscore">LEAGUE CONSTITUTION</h1>
     
     <h2 class="noUnderscore">TABLE OF CONTENTS</h2>
     
@@ -135,7 +135,7 @@
     
     <h3 class="subSectionHeading" bind:this={oneOne}>1.1 Roster Structure</h3>
     
-    <p>Rosters will be composed of 24 main roster spots (10 starters and 14 bench spots), 2 IR spots, and 4 Taxi squad spots.</p>
+    <p>Rosters will be composed of 23 active roster spots (12 starters and 11 bench spots), 2 IR spots, and 4 Taxi squad spots.</p>
     
     <h3 class="subSectionHeading" bind:this={oneTwo}>1.2 Starting Lineup</h3>
     
@@ -148,16 +148,16 @@
         <li>WR</li>
         <li>WR</li>
         <li>TE</li>
-        <li>FLEX (RB/WR/TE)</li>
-        <li>FLEX (RB/WR/TE)</li>
-        <li>SUPERFLEX (QB/RB/WR/TE)</li>
+        <li>Flex (RB/WR/TE)</li>
+        <li>Fles (RB/WR/TE)</li>
+        <li>Superflex (QB/RB/WR/TE)</li>
+        <li>K</li>
+        <li>DST</li>
     </ul>
     
-    <p>There are no kickers or defenses.</p>
-
     <h3 class="subSectionHeading" bind:this={oneThree}>1.3 Taxi Squad</h3>
     
-    <p>The Taxi Squad (aka Practice Squad) is a Sleeper default setting that gives members incentive to hold onto rookie bums who won't play for a while. There are 4 taxi squad spots. Players can only be added to the taxi squad prior to the beginning of their rookie season, but once on the squad, they can stay there through their second year. Players can be activated to the main roster at any time, but, once activated, cannot be put on the taxi squad again.</p>
+    <p>The Taxi Squad is a Sleeper default setting that gives members incentive to hold on to rookie bums who won't play for a while. There are 4 taxi squad spots. *Players can only be added to the taxi squad prior to the beginning of their rookie season, but once on the squad, they can stay there through their second year.* Players can be activated to the main roster at any time, but, once activated, cannot be put on the taxi squad again.</p>
     
     <h2 class="sectionHeading" bind:this={two}>Section 2: Trading</h2>
     
@@ -168,14 +168,14 @@
     <p>The trade deadline is Week 13 (two weeks before the playoffs start) and the trade window will re-open during the offseason.</p>
     
     <h3 class="subSectionHeading" bind:this={twoThree}>2.3 Draft Pick Trading</h3>
-    <p>Draft picks are eligible to be traded up to two years in advance. For instance, this year you will be able to trade picks in the 2025 and 2026 rookie drafts. You need to pay the buy-in for a year to be able to trade picks. For example, if you have bought in for 2024 but want to trade for (or trade away) a 2025 pick, you need to commit to paying 2025 buy-in too.</p>
+    <p>Draft picks are eligible to be traded up to two years in advance. For instance, this year you will be able to trade picks in the 2025 and 2026 rookie drafts. You must pay the buy-in for any year in which you are trading or acquiriing a pick. For example, if you have bought in for 2024 but want to trade for (or trade away) a 2025 pick, you need to commit to paying 2025 buy-in too. We are all friends so you won't be required to pay for future years immediately, but you will be held to this.</p>
     
     <h2 class="sectionHeading" bind:this={three}>Section 3: Waiver Wire</h2>
     
     <h3 class="subSectionHeading" bind:this={threeOne}>3.1 FAAB System</h3>
-    <p>The league waivers will be FAAB (free agency acquisition budget) bidding, which means that each player will be allocated $200 to spend on waiver wire pickups throughout the season.</p>
+    <p>The league waivers will be FAAB. Each player will be allocated $200 to spend on waiver wire pickups throughout the season. This amount will reset when waivers re-open for the first timeafter the Super Bowl.</p>
     
-    <p>Players on waivers will be locked upon the start of their week's game. During the season, waivers will clear at 3AM Wednesday. If a player is dropped to the waiver wire, they will be ineligible to be picked up for two days, even if waivers have cleared for the week. In the offseason, waivers will be processed once every week on Wednesdays.</p>
+    <p>Players on waivers will be locked upon the start of their week's game. During the season, waivers will clear Wednesday morning. If a player is dropped to the waiver wire, they will be ineligible to be picked up for two days, even if waivers have cleared for the week. In the offseason, waivers will be processed once every week on Wednesdays.</p>
     
     <h2 class="sectionHeading" bind:this={four}>Section 4: Rookie Draft</h2>
     
@@ -193,7 +193,7 @@
     <h2 class="sectionHeading" bind:this={five}>Section 5: Playoffs</h2>
     
     <h3 class="subSectionHeading" bind:this={fiveOne}>5.1 Playoff Format</h3>
-    <p>The playoff format will be the same as the redraft league—6 teams, with the top two teams receiving a bye. The playoffs will commence Week 15 and end Week 17. Each round will be one week. Like in the redraft league, division winners will be guaranteed a playoff spot but seeding will be based on record.</p>
+    <p>The playoff format will be the same as the redraft league—6 teams, with the top two teams receiving a bye. The playoffs will commence Week 15 and end Week 17. Each round will be one week. As there are no divisions, seeding will be based on record.</p>
     
     <h2 class="sectionHeading" bind:this={six}>Section 6: Scoring</h2>
     
@@ -209,12 +209,15 @@
     <h2 class="sectionHeading" bind:this={seven}>Section 7: League Finances</h2>
     
     <h3 class="subSectionHeading" bind:this={sevenOne}>7.1 Buy-In</h3>
-    <p>This is tricky. Want to make it small enough that it's not a financial burden for people in a lot of leagues (or just throwing money away for some of you bums) but high enough that everyone is incentivized to try. I woulda said 50 but someone said "Im in so many goddam leagues I cant do this if it's 50 or something." My proposal would be 25 this year and 5 more every subsequent year or something like that.</p>
+    <p>The buy-in is $30, to be paid in full at the start of the season.</p>
     
-    <h3 class="subSectionHeading" bind:this={sevenTwo}>7.2 Payout</h3>
-    <p>Unless people want something unique, this is probably standard: refunded entry fee for third place, 2x entry for second, rest to first place.</p>
+    <h3 class="subSectionHeading" bind:this={sevenTwo}>7.2 Season Payout</h3>
+    <p>Of the $30 buy-in, $25 will be allocated to the current season, for a total pot of $300. Third place will receive $25, second place will receive $50, and first place will receive the remaining $225.</p>
     
-    <h3 class="subSectionHeading" bind:this={sevenThree}>7.3 Leaving the League</h3>
-    <p>Not allowed. Jk, if you no longer want to be involved (#lame) that's fine, but it is your responsibility to find a replacement owner. In a lot of cases, it is hard to find someone to take over a shitty team, so you might need to pay the entry fee for the following year to make a successful transition. Just be aware of that.</p>
+    <h3 class="subSectionHeading" bind:this={sevenTwo}>7.3 Empire Payout</h3>
+    <p>The remaining $5 per person (totaling $60 per season) will be allocated to the empire pot. If someone triggers the empire clause, they will win the entire empire pot (minimum $120) in addition to first place payout.</p>
+
+    <h3 class="subSectionHeading" bind:this={sevenThree}>7.4 Leaving the League</h3>
+    <p>Not allowed. Jk, if you no longer want to be involved that's fine, but it is your responsibility to find a replacement owner. In a lot of cases, it is hard to find someone to take over a shitty team, so you might need to pay the entry fee for the following year to make a successful transition. Just be aware of that. Remember that the league will also reset, at which point you will be able to leave without needing to transition to a new owner.</p>
 
 </div>
